3cc8e43323d7d234252ae8aa7d4cf9ec99b65fc6
Carlos <carlosoftwar@gmail.com>
2021-02-20 19:24:22 -0300

    Update : Added Graph Class and Functions

--- script/grid.js
+++ script/grid.js
@@ -2,6 +2,121 @@ let labirinto = document.querySelector('.labirinto');
 let ctx = labirinto.getContext('2d');
 let atual;
 
+class Graph {
+
+  constructor(vertex){
+      this.vertex = vertex;
+      this.listAdj = new Map();   
+  }
+
+  getKeys(){
+      return this.listAdj.keys();
+  }
+  getValues(i){
+      return this.listAdj.get(i);
+  }
+  addVertex(v){   
+      this.listAdj.set(v, []);
+  }
+  addEdge(v, w){
+      this.listAdj.get(v).push(w);
+      //this.listAdj.get(w).push(v);
+  }
+  print(){
+     var vertex = this.listAdj.keys();
+     console.log("VERTEX -> LIST\n")
+      for (var i of vertex)
+      {
+          var listAdj = this.listAdj.get(i);
+          var sum = "";
+          for (var j=0; j<listAdj.length; ++j)
+          {
+              sum += listAdj[j];
+              if(j!=listAdj.length-1)sum+=" && ";
+              
+          }
+          console.log(i + " -> " + sum);
+      }
+  }
+  get Keys(){
+    return this.listAdj.keys();
+  }
+  bfs(v){
+      var fila = [];
+      var visitados = []; // vetor de visitados
+  
+      for(var i = 0; i < this.vertex; ++i)visitados[i] = false;
+
+      fila.push(v);
+      visitados[v] = true; // marca como visitado
+  
+      while(fila.length!=0){
+              var getElement = fila.shift();
+              console.log(getElement); // Vertice atual 
+              var get_values = this.listAdj.get(getElement); // Get adjList
+              for(var i in get_values){
+                  var value = get_values[i];
+                  if(!visitados[value]){
+                      //console.log(getElement); 
+                      visitados[value] = true;
+                      fila.push(value);
+                  }
+              } 
+      }
+  } 
+  dfs (v){
+      var visitados = [];
+      this.path(v, visitados);
+  }
+  path(v, visitados){
+      visitados[v] = true;
+      console.log(v); // Vertice atual
+
+      var get_values = this.listAdj.get(v);
+
+      for(var w in get_values){
+          var value = get_values[w];
+          if(!visitados[value])this.path(value, visitados)
+      }
+  }
+  findPath(origem, destino) {
+    if (origem == destino) {
+      return [origem, destino]
+    }
+    var fila = [origem],visitados = {}, ant = {}, aux = 0, steps ;
+
+    while(aux < fila.length) {
+      var u = fila[aux];
+      aux++;
+      if (!this.listAdj.get(u)) {
+        continue;
+      }
+
+      var get_values = this.listAdj.get(u);
+      for(var i = 0; i < get_values.length; ++i) {
+        var v = get_values[i];
+        if (visitados[v]) {
+          continue;
+        }
+        visitados[v] = true;
+        if (v === destino) { 
+          steps = [v];
+          while (u !== origem) {
+            steps.push(u);
+            u = ant[u];
+          }
+          steps.push(u);
+          steps.reverse();
+          return steps;
+        }
+        ant[v] = u;
+        fila.push(v);
+      }
+    }
+    return steps;
+  }
+
+}
 class Labirinto {
   constructor(tamanho, linhas, colunas) {
     this.tamanho = tamanho;
